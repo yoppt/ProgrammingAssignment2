9fde50d80d0a1eb52ee5d5a8c7752e181d83acad
yoppt <hi@alexeygaponov.ru>
2015-09-26 14:08:40 +0200

    homework done

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,40 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## Create a matrix with a cacheable inverse via getters/setters
 
 makeCacheMatrix <- function(x = matrix()) {
+
+  inv <- NULL #stub for inverse matrix
+  
+  #getters
+  get <- function() x
+  getinv <- function() inv
+  
+  #setters
+  set <- function(y) {
+    x <<- y
+    inv <<- NULL
+  }
+  setinv <- function(invmx) inv <<- invmx
+  
+  list(set = set, get = get, setinv = setinv, getinv = getinv)
 
 }
 
 
-## Write a short comment describing this function
+## calculate the mx inverse or get it from cache and return
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Returns a matrix that is the inverse of 'x'
+  
+  inv <- x$getinv() #try getting the cached inverse
+  
+  if(!is.null(inv)) { #avail in cache
+    message("getting inverse matrix from cache")
+    return(inv)   #function exits
+  }
+  
+  #not cached - do it the hard way  
+  data <- x$get()
+  inv <- solve(data, ...) #throws errors when fed with non-square
+  x$setinv(inv) #save to cache
+  inv
 }
